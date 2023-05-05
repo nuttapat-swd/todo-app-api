5503a8186930b9ddc09b48a9845c2217a45e7ad8
Nuttapat Chakpech <nuttapat.swd@gmail.com>
2023-05-05 17:16:31 +0700

    move emd point

--- todoApi/user/urls.py
+++ todoApi/user/urls.py
@@ -4,5 +4,5 @@ from user import views
 
 urlpatterns = [
     path('users',views.UserListView.as_view(),name='user-list'),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    
 ]
