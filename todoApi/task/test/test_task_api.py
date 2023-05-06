1277a582b56ea6036a087dc7e5337ae109cb25f3
nuttapat115 <nuttapat115_@hotmail.com>
2023-05-06 09:56:19 +0700

    added auth test

--- todoApi/task/test/test_task_api.py
+++ todoApi/task/test/test_task_api.py
@@ -1,17 +1,36 @@
-# from rest_framework.test import APIClient
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# from django.test import TestCase
-# from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# from task import serializers
+from task import serializers
+from core.models import Task
 
-# class PublicTaskTest(TestCase):
+TASK_URL = reverse('task:task-list')
+
+def create_task(user, **params):
+    """Create and retuen a new task"""
+    default_task = {
+        'title': 'Test Task',
+        'description': 'Test Task Description',
+    }
+    default_task.update(params)
+
+    task = Task.objects.create(owner=user, **default_task)
+    return task
 
 
-#     def create_task(self, **params):
-#         default = {
-            
-#         }
+class PublicTaskTest(TestCase):
+    """Test unauthenticated get task list"""
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(TASK_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
 
 
 #     def test_create_task_by_serialization(self):
